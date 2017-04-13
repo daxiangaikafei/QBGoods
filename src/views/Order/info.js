901c16b82d53027a4de71de1b38f55c861cf5b93
nani <nanijser@gmail.com>
2017-04-13 10:32:17 +0800

    order status edit

--- src/views/Order/info.js
+++ src/views/Order/info.js
@@ -15,6 +15,20 @@ class Info extends React.Component {
 
     render() {
         let {data,onClick} = this.props;
+        let statusBack = '';
+        switch (data.rebateStatus) {
+          case 1:
+            statusBack = <li className="info-select">返还宝券</li>;
+            break;
+          case 2:
+            statusBack = <li className="info-select">返券被收回</li>;
+            break;
+          case 3:
+            statusBack = <li className="info-select">返券失败</li>;
+            break;
+          default:
+            statusBack = <li>返还宝券</li>;
+        }
         return (
             <div onClick={onClick} className="container-my-order-info">
 
@@ -22,9 +36,7 @@ class Info extends React.Component {
                         <li>下单</li>
                         <li className={data.rebateStatus==-1?"info-select":""}>记录订单</li>
                         <li className={data.rebateStatus==0?"info-select":""}>待返宝券</li>
-                        <li className={data.rebateStatus==1?"info-select":""}>返还宝券</li>
-                        <li className={data.rebateStatus==2?"info-select":""}>返券被收回</li>
-                        <li className={data.rebateStatus==3?"info-select":""}>返券失败</li>
+                        { statusBack }
                     </ol>
                     <div className="order-info-content">
                         <p><label>商城：</label>{data.source||""}</p>
