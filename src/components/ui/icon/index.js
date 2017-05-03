067d64439a2c01de7188b543509b4214a373b4e5
FiniYang <fini.yang@gmail.com>
2017-05-03 14:41:11 +0800

    update Icon with stateless function

--- src/components/ui/icon/index.js
+++ src/components/ui/icon/index.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import classNames from 'classnames'
 import './index.less'
 
-class Icon extends Component {
-  url = require(`./symbol-defs.svg`)
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    let { name, size, color, style, className } = this.props
-    color = color || '#000'
-    size = size || 16
-    name = `icon-${this.props.name}`
-    return (
-      <svg
-        style={{
-          width: size + 'px',
-          height: size + 'px',
-          fill: color,
-          ...style
-        }} 
-        className={classNames({icon:1,[className]:1})}>
-        {<use xlinkHref={`${this.url}#${name}`}></use>}
-      </svg>
-    )
-  }
+const Icon = props => {
+  let url = require(`./symbol-defs.svg`)
+  let { name, size, color, style, className } = props
+  name = `icon-${name}`
+  return (
+    <svg
+      style={{
+        width: size + 'px',
+        height: size + 'px',
+        fill: color,
+        ...style
+      }} 
+      className={classNames({icon:1,[className]:1})}>
+      {<use xlinkHref={`${url}#${name}`}></use>}
+    </svg>
+  )
 }
-
+Icon.defaultProps = {
+  color: '#000',
+  size: 16
+}
 export default Icon
