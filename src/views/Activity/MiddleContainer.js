44f3df2ea3c95bdbe26b6f2960cba444f7b4a8b8
FiniYang <fini.yang@gmail.com>
2017-05-19 11:15:43 +0800

    MiddleContainer update

--- src/views/Activity/MiddleContainer.js
+++ src/views/Activity/MiddleContainer.js
@@ -3,23 +3,11 @@ import CSSModules from 'react-css-modules'
 import styles from './channelEntry.less'
 import { Link } from 'react-router'
 import classNames from 'classnames'
-import { priceFormat, eventFun } from 'libs/util'
+import { priceFormat, eventFun, icons } from 'libs/util'
+import MiddleTitle from './MiddleTitle'
 
 class MiddleContainer extends Component {
-  pageName = '108'
-  icons = {
-    'tmall': require('static/imgs/thirdSource/tmall.png'),
-    'dangdang': require('static/imgs/thirdSource/dangdang.png'),
-    'gome': require('static/imgs/thirdSource/gome.png'),
-    'jd': require('static/imgs/thirdSource/jd.png'),
-    'jumei': require('static/imgs/thirdSource/jumei.png'),
-    'kaola': require('static/imgs/thirdSource/kaola.png'),
-    'mi': require('static/imgs/thirdSource/mi.png'),
-    'taobao': require('static/imgs/thirdSource/taobao.png'),
-    'yhd': require('static/imgs/thirdSource/yhd.png'),
-    'yougou': require('static/imgs/thirdSource/yougou.png'),
-    'qbao': require('static/imgs/thirdSource/qbao.png'),
-  }
+
   constructor(props) {
     super(props);
   }
@@ -28,19 +16,20 @@ class MiddleContainer extends Component {
   componentDidUpdate() {
   }
   render() {
-    var level0s = this.props.levelData;
+    let { levelData, pageName, modelName, today } = this.props;
+    if(levelData.stuffs.length == 0) return ''
     return (
       <div>
-        <div styleName="middle-title">{level0s.title}</div>
+        <MiddleTitle title={levelData.title} today />
         <div styleName="middle-container">
-          {/*style={{ width: `${140 * level0s.stuffs.length}px`}}*/}
-          <div styleName="list" style={{ width: `${140 * level0s.stuffs.length}px`}}>
-            {level0s.stuffs.map((item, index) =>
+          {/*style={{ width: `${140 * levelData.stuffs.length}px`}}*/}
+          <div styleName="list" style={{ width: `${140 * levelData.stuffs.length}px`}}>
+            {levelData.stuffs.map((item, index) =>
               <div styleName="item" key={index}>
-                <a {...eventFun(this.pageName,'channel_entry_ad_products',item.id)} styleName="img" href={item.url} ><img src={item.imgUrl} alt="" /></a>
-                <a {...eventFun(this.pageName,'channel_entry_ad_products',item.id)} href={item.url} ><h3>{item.name}</h3></a>
+                <a {...eventFun(pageName,modelName,item.id)} styleName="img" href={item.url} ><img src={item.imgUrl} alt="" /></a>
+                <a {...eventFun(pageName,modelName,item.id)} href={item.url} ><h3>{item.name}</h3></a>
                 <div styleName="price">￥{priceFormat(item.finalPrice)}
-                  <span styleName="icon"><img src={this.icons[item.source]} alt=""/></span>
+                  <span styleName="icon"><img src={icons[item.source]} alt=""/></span>
                 </div>
                 <div styleName="bottom">
                   <span styleName="return">{item.rebateValue}</span>
