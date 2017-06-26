1d009e9831125a4b5f711b74d80c5fae951ee914
FiniYang <fini.yang@gmail.com>
2017-06-26 21:37:06 +0800

    Order update

--- src/views/Order/noOrder.js
+++ src/views/Order/noOrder.js
@@ -19,7 +19,7 @@ class NoOrder extends React.Component {
             <div className="container-no-order">
                 <div></div>
                 <p dangerouslySetInnerHTML={{__html: this.props.tipText || '亲，购物才会返返利哟'}}></p>
-                <a href="newTab://goodstuff.qbao.com/search">立即前去</a>
+                <a href="newTab://goodstuff.qbao.com/search">立即前去 </a>
             </div>
         )
     }
