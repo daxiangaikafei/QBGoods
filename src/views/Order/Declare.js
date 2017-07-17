8d842ce59f4ba1565f5b9e7949c21581816f4a2f
FiniYang <fini.yang@gmail.com>
2017-07-17 17:09:05 +0800

    Decalar commit

---
+++ src/views/Order/Declare.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import classNames from 'classnames'
+import './declare.less'
+
+const Declare = props => {
+  return (
+    <div className="declare-container">
+      <div className="qrcode">
+      </div>
+      <div className="declare">
+        <h4>有好货在<span>20日</span>会正式更名为<span>“我有好物”</span>欢迎大家关注我们</h4>
+        <p>1、在微信里面搜索公众号“我有好物”</p>
+        <p>2、在微信客户里面咨询申诉</p>
+      </div>
+    </div>
+  )
+}
+
+export default Declare
