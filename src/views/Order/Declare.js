31a752749042ee8d989c1822142731c6ea394277
FiniYang <fini.yang@gmail.com>
2017-08-02 10:36:00 +0800

    Declare txt update

--- src/views/Order/Declare.js
+++ src/views/Order/Declare.js
@@ -8,7 +8,7 @@ const Declare = props => {
       <div className="qrcode">
       </div>
       <div className="declare">
-        <h4>有好货在<span>20日</span>会正式更名为<span>“我有好物”</span>欢迎大家关注我们</h4>
+        <h4>有好货正式更名为<span>“我有好物”</span>欢迎大家关注我们</h4>
         <p>1、在微信里面搜索公众号“我有好物”</p>
         <p>2、在微信客户里面咨询申诉</p>
       </div>
