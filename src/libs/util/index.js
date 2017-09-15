c8667af59037fe89fe829d8b4c929bfab537c0c5
FiniYang <fini.yang@gmail.com>
2017-09-15 10:04:16 +0800

    Util update

--- src/libs/util/index.js
+++ src/libs/util/index.js
@@ -157,3 +157,13 @@ export function showSourceTip(url,source,PopUp,e){
   
   e.preventDefault();
 }
+
+export function getParamByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
