1fe5a8467709b34c3e26aea45962640b339865a5
FiniYang <fini.yang@gmail.com>
2017-09-19 16:49:35 +0800

    Home update

--- src/views/Home/page.js
+++ src/views/Home/page.js
@@ -7,13 +7,21 @@ import classNames from 'classnames'
 import { getCookie, setCookie, priceFormat } from 'libs/util'
 import { fetchPosts } from "components/common/fetch"
 import ReactSwipe from 'react-swipe';
-
+const bannerList = [
+  {
+    imgUrl: require('static/imgs/activity/banner2.jpg')
+  },
+  {
+    imgUrl: require('static/imgs/activity/banner3.jpg')
+  },
+]
 class Home extends Component {
 
   constructor(props) {
     super(props)
 
     this.state = {
+      bannerActive:0,
     }
   }
 
@@ -29,7 +37,23 @@ class Home extends Component {
   render() {
     return (
       <div styleName="home-container" style={this.props.style}>
-        
+        <div className="search">
+          <div className="input">
+            
+          </div>
+        </div>
+        <div styleName="banner">
+          <ReactSwipe ref="swiper" swipeOptions={{continuous: false, callback: bannerActive=>{this.setState({bannerActive})}}}>
+            {bannerList.map((item, index) => (
+              <img src={item.imgUrl} key={index} onClick={()=>{}}/>
+            ))}
+          </ReactSwipe>
+          <div styleName="swiper-pagination">
+            {
+              bannerList.map((item,i)=> <span key={i} styleName={classNames('swiper-pagination-bullet',{'swiper-pagination-bullet-active':this.state.bannerActive == i})}></span>)
+            }
+          </div>
+      </div>
       </div>
     )
   }
